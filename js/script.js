0a0bb31bee50f1885a472775c9e20dfc01907700
legal-blind <dgray11@hawk.iit.edu>
2025-04-15 16:44:55 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
+function initMap() {
+  const center = { lat: 41.5834, lng: -87.2540 }; // Hobart, IN
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: center,
+  });
 
+  // Custom Marker
+  const marker = new google.maps.Marker({
+    position: center,
+    map: map,
+    title: "Our Main Office",
+    icon: {
+      url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+    },
+  });
+
+  // Info Window
+  const infoWindow = new google.maps.InfoWindow({
+    content: "<h3>Our Main Office</h3><p>Come visit us!</p>",
+  });
+
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
+
+  // Directions Link
+  const directionsLink = document.createElement("a");
+  directionsLink.href = `https://www.google.com/maps/dir/?api=1&destination=${center.lat},${center.lng}`;
+  directionsLink.textContent = "Get Directions";
+  directionsLink.target = "_blank";
+  document.body.appendChild(directionsLink);
+}
