ad7d161494cbe0e74d5dd3bb953d6591c62444e7
legal-blind <dgray11@hawk.iit.edu>
2025-04-29 13:54:37 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-function goUp() {
-  const buttn = document.getElementById("toTop");
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    buttn.style.display = "block";
-  } else {
-    buttn.style.display = "none";
-  }
-}
-
-document.getElementById("toTop").onclick = function goUp() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-};
-
-document.getElementById("toggleColor").addEventListener("click", function () {
-  document.body.classList.toggle("dark-mode");
-});
-
 function initMap() {
   const center = { lat: 41.8349, lng: -87.6270 }; // Illinois Tech
 
